c898f6bcce372c66a2721c5761397187c344d5af
Natalia Gonzalez <natigb@estudiantec.cr>
2022-10-23 21:21:16 -0600

    [Add] React setup and initial match forms

---
+++ wconline-match/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import{BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import MatchForm from './components/MatchForm';
+function App() {
+  return (
+    <div className="App">
+        <h1>Hihihi</h1>
+        <MatchForm />
+    </div>
+  );
+}
+
+export default App;
