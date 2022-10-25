cfd103a0ebabf52424d46bbfb1d3d76abfca33eb
Dennis Jimenez <alejimenezc@hotmail.com>
2022-10-25 15:15:37 -0600

    [Add] Tournament post

--- wconline-match/src/App.js
+++ wconline-match/src/App.js
@@ -2,11 +2,12 @@
 import './App.css';
 import{BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import CreateMatch from './components/CreateMatch';
+import CreateTournament from './components/CreateTournament';
 function App() {
   return (
     <div className="App">
         <h1>Hihihi</h1>
-        <CreateMatch />
+        <CreateTournament />
     </div>
   );
 }
