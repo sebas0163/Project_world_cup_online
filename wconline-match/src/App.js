824f33d1001a5205c6626932e7aa5033f3af1a3f
Dennis Jimenez <alejimenezc@hotmail.com>
2022-10-25 20:58:07 -0600

    [Update] Tourney Display

--- wconline-match/src/App.js
+++ wconline-match/src/App.js
@@ -1,14 +1,18 @@
 
+//<CreateMatch />
+//<ViewMatches />
+//<CreateTournament/>
+//<TournamentDisplays/>
 import './App.css';
 import{BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import CreateMatch from './components/CreateMatch';
 import CreateTournament from './components/CreateTournament';
 import ViewMatches from './components/ViewMatches';
+import TournamentDisplays from './components/TournamentDisplay';
 function App() {
   return (
     <div className="App">
         <h1>World Cup Online</h1>
-        <CreateMatch />
         <ViewMatches />
     </div>
   );
--- wconline-match/src/components/CreateTournament.js
+++ wconline-match/src/components/CreateTournament.js
@@ -4,7 +4,6 @@ import axios from "axios";
 
 function TourneyForm(){
     const url = "http://localhost:5000/api/v1/tournament/"
-    const url2 = "http://localhost:5000/api/v1/stage/"
     const type = [
         { value: '', text: 'Escoja el tipo de torneo' },
         { value: 'Selecciones', text: 'Selecciones' },
@@ -43,50 +42,11 @@ function TourneyForm(){
         setData(newData)
         console.log(newData)
     }
-
-    const [stageData, setData2] = useState({
-        
-        Id: "",
-        Name: "",
-        StartDate: "",
-        EndDate: "",
-        Tournament_ID: ""
-    })
-
-    function submit2(e){
-        e.preventDefault();
-        console.log(url2)
-        axios.post(url2, {
-            Id: stageData.Id,
-            Name: stageData.Name,
-            StartDate: stageData.StartDate,
-            EndDate: stageData.EndDate,
-            Tournament_ID: stageData.Tournament_ID
-        })
-        .then(response =>{
-            console.log(response.tourneyData)
-        })
-    }
-
-    function handle2(e){
-        const newData = {...stageData}
-        newData[e.target.id] = e.target.value
-        setData2(newData)
-        console.log(newData)
-    }
     return(
         <div>
-
-            <form onSubmit={(e)=>submit2(e)}>
-            <input onChange = {(e)=>handle2(e)} id = "StartDate" value = {stageData.StartDate} placeholder ="StartDate" type="datetime-local"></input>
-            <input onChange = {(e)=>handle2(e)} id = "EndDate" value = {stageData.EndDate} placeholder ="EndDate" type="datetime-local"></input>
-            <input onChange = {(e)=>handle2(e)} id = "Name" value = {stageData.Name} placeholder ="Stage_name" type="text"></input>
-            <button type = "cstage"> Crear fase</button>
-            </form>
-
             <form onSubmit={(e)=>submit(e)}>
-            <input onChange = {(e)=>handle(e)} id = "StartDate" value = {tourneyData.StartDate} placeholder ="StartDate" type="datetime-local"></input>
-            <input onChange = {(e)=>handle(e)} id = "EndDate" value = {tourneyData.EndDate} placeholder ="EndDate" type="datetime-local"></input>
+            <input onChange = {(e)=>handle(e)} id = "StartDate" value = {tourneyData.StartDate} placeholder ="StartDate" type="date"></input>
+            <input onChange = {(e)=>handle(e)} id = "EndDate" value = {tourneyData.EndDate} placeholder ="EndDate" type="date"></input>
             <input onChange = {(e)=>handle(e)} id = "Name" value = {tourneyData.Name} placeholder ="Name" type="text"></input>
             <textarea onChange = {(e)=>handle(e)} id = "Rules" value = {tourneyData.Rules} placeholder ="Rules" type="text"></textarea>
             <div>
