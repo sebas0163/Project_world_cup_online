7b59fab11e1983375c0fa713bb8df850d39b71d3
Steve <stevealv@hotmail.com>
2022-10-27 16:07:36 -0600

    [Update] Home, Create Tournament and Match

--- Frontend/world-cup-online/src/components/home.js
+++ Frontend/world-cup-online/src/components/home.js
@@ -9,29 +9,29 @@ const Home = props => {
     return (
         <>
             <div className="Home">
-                <br></br>
-                <div className="row">
-                    <div className="col-12">
-                        <h1 id="titleHome">Esta es la homepage. Un titulo
-                            creativo va aqui, por favor modicar en el futuro</h1>
+                <div className="wrapperHome" >
+                    <br></br>
+                    <div className="row">
+                        <div className="col-12">
+
+                            <h1 id="titleHome">Bienvenido a World Cup Online.
+                                Ahora el fútbol está más cerca que nunca.</h1>
+
+                        </div>
+                    </div>
+                    <br></br><br></br>
+                    <div className="row">
+                        <div className="col-6">
+                            <button id="goldBtn" className="btn btn-warning" onClick={() => navigate("/view-match")}>Dasboard de partidos</button>
+                        </div>
+                    </div>
+                    <br></br><br></br>
+                    <div className="row">
+                        <div className="col-6">
+                            <button id="goldBtn" className="btn btn-warning" onClick={() => navigate("/tournament-display")}>Dashboard de torneos</button>
+                        </div>
                     </div>
                 </div>
-
-                <br></br><br></br>
-                <div className="row">
-                    <div className="col-6">
-                        <button id="goldBtn" className="btn btn-warning" onClick={() => navigate("/view-match")}>Dasboard de partidos</button>
-                    </div>
-                </div>
-                <br></br><br></br>
-                <div className="row">
-                    <div className="col-6">
-                        <button id="goldBtn" className="btn btn-warning" onClick={() => navigate("/tournament-display")}>Dashboard de torneos</button>
-                    </div>
-                </div>
-
-
-
             </div>
         </>
     )
