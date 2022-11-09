be09724f04192152886ddf1c633ad2bcacb6a8fc
Steve <stevealv@hotmail.com>
2022-11-08 18:34:08 -0600

    [Add] CreatePrediction

--- API/controllers/prediction.controller.ts
+++ API/controllers/prediction.controller.ts
@@ -20,10 +20,16 @@ export default class PredictionController {
 
     static async createPrediction(req: Request, res: Response) {
         try {
-            const { Home_Score, Visit_Score, Best_player, Id_user, Id_match } = req.body;
+            const { Home_Score, Visit_Score, Best_player, Id_user, Id_match,
+                GoalList } = req.body;
+            if (!Home_Score || !Visit_Score || !Best_player || !Id_user || !Id_match || !GoalList) {
+                res.status(400).json({ msg: 'Please enter all fields' });
+                return;
+            }
             const pool = await poolPromise;
             const predictionRepository: IPredictionRepository = new PredictionRepository(pool);
-            const result = await predictionRepository.createPrediction(Home_Score, Visit_Score, Best_player, Id_user, Id_match);
+            const result = await predictionRepository.createPrediction(+Home_Score, +Visit_Score,
+                +Best_player, +Id_user, +Id_match, GoalList);
             res.status(200).json(result);
         }
         catch (err) {
--- API/repositories/interfaces/prediction.interface.ts
+++ API/repositories/interfaces/prediction.interface.ts
@@ -2,6 +2,7 @@ import { Prediction } from "../../models/prediction";
 
 export interface IPredictionRepository {
     getPredictions(): Promise<Prediction[]>;
-    createPrediction(Home_Score: number, Visit_Score: number, Best_player: number,
-        Id_user: number, Id_match: number): Promise<number>;
+    createPrediction(Home_Score: number, Visit_Score: number,
+        Best_player: number, Id_user: number, Id_match: number,
+        GoalList: { Goal_Scorer: string, Attendee: string }[]): Promise<number>;
 }
