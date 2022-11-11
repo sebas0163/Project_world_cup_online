0f3eb389431a01ec7f2038d5db4403ad1a13d663
Dennis Jimenez <alejimenezc@hotmail.com>
2022-11-10 23:02:40 -0600

    [Update] Request

--- Frontend/world-cup-online/src/components/login.js
+++ Frontend/world-cup-online/src/components/login.js
@@ -3,8 +3,53 @@ import "./login.css";
 import { useState, useEffect } from 'react';
 import { useNavigate } from "react-router-dom";
 import placeholderPNG from '../assets/images/placeholder.png';
+import axios from "axios";
 
 const Login = props => {
+
+    const url = "http://localhost:5000/api/v1/user/login"
+
+    function validateLogin() {
+        if (loginData.Email.length == 0 ||
+            loginData.Password.length == 0) {
+
+            return false
+
+        }
+        return true
+    }
+    const[loginData, setLoginData] = useState({
+        Email: "",
+        Password: ""
+    })
+
+    function handleLoginInput(e) {
+        const newData = { ...loginData }
+        newData[e.target.id] = e.target.value
+        setLoginData(newData)
+        console.log(newData)
+    }
+
+    function submitLogin(e) {
+        e.preventDefault();
+        if(validateLogin()){
+            console.log(url)
+            axios.post(url, {
+                Email: loginData.Email,
+                Password: loginData.Password
+            })
+                .then(response => {
+                    console.log("Success")
+                })
+            alert("Bienvenido")
+            navigate("/home")
+        }
+        else{
+            alert(`Por favor llene todos los espacios`)
+        }
+    }
+
+
     const navigate = useNavigate();
     return (
         <>
@@ -25,7 +70,7 @@ const Login = props => {
                         </div>
                         <div className="row">
                             <div className="col-auto">
-                                <input type="text" placeholder="Email"></input>
+                                <input type="text" onChange={(e) => handleLoginInput(e)} value={loginData.Email} placeholder="Email" id="Email"></input>
                             </div>
                         </div>
                         <br />
@@ -36,12 +81,12 @@ const Login = props => {
                         </div>
                         <div className="row">
                             <div className="col-auto">
-                                <input type="text" placeholder="Password"></input>
+                                <input type="text" onChange={(e) => handleLoginInput(e)} value={loginData.Password} placeholder="Password" id="Password"></input>
                             </div>
                         </div>
                     </div>
                     <br /><br />
-                    <div className="btn btn-warning" onClick={() => navigate("/home")}>Ingresar</div>
+                    <div className="btn btn-warning" onClick={(e) => submitLogin(e)}>Ingresar</div>
                     <br />
                     <br />
                     <p>¿No tienes cuenta? <a href="#">Resgistrate aqui.</a></p>
