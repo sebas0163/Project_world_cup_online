b3a5a32a15927b0f4e026f6d3b1d10a37b6ad75d
Natalia Gonzalez <natigb@estudiantec.cr>
2022-11-25 02:19:06 -0600

    [Add] Create Prediction test

--- Frontend/world-cup-online/src/components/__tests__/prediction/CreatePrediction.test.js
+++ Frontend/world-cup-online/src/components/__tests__/prediction/CreatePrediction.test.js
@@ -1,13 +1,38 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
 import CreatePrediction from '../../prediction/CreatePrediction';
-
+import { render, screen } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
+import { userDummy, matchDummy } from '../utils/dummies';
+const mockChildComponent = jest.fn();
+
+
+jest.mock("../../prediction/CreatePrediction", () => (props) => {
+  mockChildComponent(props);
+  return <mock-childComponent />;
+});
 
 test("should render CreatePrediction component", () =>{
     const div = document.createElement('div');
     ReactDOM.render(<BrowserRouter>
                      <CreatePrediction/>
                     </BrowserRouter>,div)
+    //render(<CreatePrediction user={user} match={match} mode={userType}/>)
+});
 
-})
+test("If ParentComponent is passed open and has data, ChildComponent is called with prop open and data", () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<BrowserRouter>
+        <CreatePrediction user = {userDummy()} match = {matchDummy()} mode = "user" />
+       </BrowserRouter>,div)
+  
+    // Check that the Jest mock function is called with an object.
+    // Use 'expect.objectContaining'
+    expect(mockChildComponent).toHaveBeenCalledWith(
+      expect.objectContaining({
+        user: userDummy(),
+        match: matchDummy(),
+        mode : "user"
+      }) 
+    ); 
+  });
---
+++ Frontend/world-cup-online/src/components/__tests__/utils/dummies.js
@@ -0,0 +1,26 @@
+export function userDummy(){
+    const user = {
+        "Id":1,
+        "Name":"nati",
+        "NickName":"nati",
+        "Email":"nati",
+        "Password": "1234",
+        "Birthdate":"2001-02-08T00:00:00.000Z"
+    }
+    return user;
+} 
+
+export function matchDummy(){
+    const match = {
+                    "Id":1,
+                    "Stadium":"estadio",
+                    "StartDateTime":"2022-11-27T11:00:00.000Z",
+                    "Tournament_ID":"zya0a4",
+                    "Stage_ID":1,
+                    "State":"Pendiente",
+                    "Score":"0-0",
+                    "HomeId":1,
+                    "VisitId":2
+                }
+    return match;
+} 
